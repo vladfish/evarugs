7de640f0545b1099e765bcb9369d03f5290a81e4
vladfish <41798467+vladfish@users.noreply.github.com>
2022-02-13 11:29:24 +0400

    Add files via upload
    homework #3

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.main-slider', {
+    slidesPerView: 2,
+    spaceBetween: 6,
+    
+  loop: true,
+
+  navigation: {
+    nextEl: '.main-slider__arrow',
+  },
+
+ 
+
+});
